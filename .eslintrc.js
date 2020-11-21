c03279143d01254d4dd775a5945e46b997b1b5a6
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-11-22 00:01:36 +0800

    chore(detox): ios setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,18 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: [
+        'e2e/**/*.{ts,tsx,js}',
+        '__tests__/**',
+        '__mocks__/**/*.ts',
+        '__mocks__/**/*.tsx',
+        'src/libs/helpers/jest/*.ts',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
