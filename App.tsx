9c70bbf711f39ae7d3d81bf6c6ca804057753bca
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-11-22 01:55:36 +0800

    chore(android): detox setup

--- App.tsx
+++ App.tsx
@@ -1,118 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare const global: {HermesInternal: null | {}};
+import React, {useState} from 'react';
+import {Text, Button, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => {
+  const [showHello, setShowHello] = useState(false);
+  const [showWorld, setShowWorld] = useState(false);
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.container}>
+      <Text accessibilityLabel="welcome" style={styles.welcomeText}>
+        Welcome to Detox + Cucumber
+      </Text>
+      {showHello ? <Text>Hello!</Text> : null}
+      {showWorld ? <Text>World!</Text> : null}
+      <Button
+        testID="hello_button"
+        title="Hello"
+        onPress={() => setShowHello((prev) => !prev)}
+      />
+      <Button
+        testID="world_button"
+        title="World"
+        onPress={() => setShowWorld((prev) => !prev)}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  welcomeText: {fontSize: 20, fontWeight: '800'},
 });
 
 export default App;
---
+++ e2e/firstTest.e2e.ts
@@ -0,0 +1,29 @@
+import {expect, device, element, by} from 'detox';
+
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.label('welcome'))).toBeVisible();
+  });
+
+  it('should toggle Hello! text visibility after tap', async () => {
+    await element(by.id('hello_button')).tap();
+    await expect(element(by.text('Hello!'))).toBeVisible();
+
+    await element(by.id('hello_button')).tap();
+    await expect(element(by.text('Hello!'))).not.toBeVisible();
+  });
+
+  it('should toggle World! text visibility after tap', async () => {
+    await element(by.id('world_button')).tap();
+    await expect(element(by.text('World!'))).toBeVisible();
+
+    await element(by.id('world_button')).tap();
+    await expect(element(by.text('World!'))).not.toBeVisible();
+  });
+});
+
+export {};
