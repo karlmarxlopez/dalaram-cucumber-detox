c9e33ded839f535878f2944b225a19f0f67fdd9c
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-11-27 03:17:31 +0800

    fix: apply work around for detox

---
+++ .detoxrc.js
@@ -0,0 +1,65 @@
+
+// This is the workaround for the cucumber CLI args pass through issue
+function setConfiguration() {
+  const PLATFORM = process.env.DETOX_PLATFORM || 'ios'; //ios | android
+  const FLAVOR = process.env.DETOX_BUILD_FLAVOR || 'release'; // debug | release
+
+  const configurations = {
+    ios_debug: {
+      'ios.sim.debug': {
+        type: 'ios.simulator',
+        build: 'xcodebuild -workspace ios/rnsandbox.xcworkspace -scheme rnsandbox -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
+        binaryPath: './ios/build/Build/Products/Debug-iphonesimulator/rnsandbox.app',
+        device: {
+          type: 'iPhone 8'
+        }
+      }
+    },
+    ios_release: {
+      'ios.sim.release': {
+        type: 'ios.simulator',
+        build: 'xcodebuild -workspace ios/rnsandbox.xcworkspace -scheme rnsandbox -configuration Release -sdk iphonesimulator -derivedDataPath ios/build',
+        binaryPath: './ios/build/Build/Products/Debug-iphonesimulator/rnsandbox.app',
+        device: {
+          type: 'iPhone 8'
+        }
+      }
+    },
+    android_debug: {
+      'android.emu.debug': {
+        type: 'android.emulator',
+        binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+        build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..',
+        device: {
+          'avdName': 'Pixel_API_29_AOSP'
+        }
+      },
+    },
+    android_release: {
+      'android.emu.release': {
+        type: 'android.emulator',
+        build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release && cd ..',
+        binaryPath: './android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk',
+        device: {
+          'avdName': 'Pixel_API_29_AOSP'
+        }
+      }
+    }
+  };
+
+  return configurations[`${PLATFORM}_${FLAVOR}`];
+}
+
+
+module.exports = {
+  testRunner: 'jest',
+  runnerConfig: 'e2e/config.json',
+  detox: {
+    behavior: {
+      init: {
+        exposeGlobals: false
+      }
+    }
+  },
+  configurations: setConfiguration(),
+}
